336fdd6f5b2db446396142ecc3463d7b9164bf61
Kirill Martynov <kirchik.martynov.995@gmail.com>
2022-05-11 01:39:21 +0300

    Feature, TKL-26, Add Board Columns, Task Card component

--- src/core/components/Sidebar/Sidebar.tsx
+++ src/core/components/Sidebar/Sidebar.tsx
@@ -12,21 +12,23 @@ export const Sidebar = () => {
 
   return (
     <div className={s.root}>
-      <div className={s.logo}>
+      <NavLink className={s.logo} to="/">
         <span>T</span>asklice
+      </NavLink>
+      <div className={s.content}>
+        {MENU.map((item) => (
+          <NavLink
+            key={item.label}
+            className={cn(s.menuItem, {
+              [s.active]: location.pathname === item.path,
+            })}
+            to={item.path}
+          >
+            <SVG src={`icons/${item.icon}.svg`} width={22} height={22} />
+            <span>{item.label}</span>
+          </NavLink>
+        ))}
       </div>
-      {MENU.map((item) => (
-        <NavLink
-          key={item.label}
-          className={cn(s.menuItem, {
-            [s.active]: location.pathname === item.path,
-          })}
-          to={item.path}
-        >
-          <SVG src={`icons/${item.icon}.svg`} width={22} height={22} />
-          <span>{item.label}</span>
-        </NavLink>
-      ))}
     </div>
   );
 };
--- vite/aliases.ts
+++ vite/aliases.ts
@@ -2,7 +2,7 @@
 const path = require('path');
 
 export const aliases = {
-  '@core': path.resolve(__dirname, 'src/core'),
-  '@tasks': path.resolve(__dirname, 'src/tasks'),
-  '@screens': path.resolve(__dirname, 'src/screens'),
+  '@core': path.resolve(__dirname, '../src/core'),
+  '@tasks': path.resolve(__dirname, '../src/tasks'),
+  '@screens': path.resolve(__dirname, '../src/screens'),
 };
